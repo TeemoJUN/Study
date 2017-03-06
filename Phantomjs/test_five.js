1c25e6f3691a94c411aa15a979815746f2787f6e
wanzailin <1058794886@qq.com>
2017-03-06 23:31:50 +0800

    new

--- Phantomjs/test_five.js
+++ Phantomjs/test_five.js
@@ -3,16 +3,17 @@ var page=require('webpage').create();
 var url="http://ife.baidu.com/course/all";
 var fs = require("fs");
 var rs;
+var time=Date.now();
 phantom.outputEncoding="gbk";
  page.includeJs(
  	'http://code.jquery.com/jquery-1.4.1.min.js',
  	function(){
  		page.onCallback=function(){
-				rs=page.evaluate(function(){
+				rs=page.evaluate(function(t){
 					var result={};
 					result.code=1;
 					result.meg="抓取成功";
-					result.time=2000;
+					result.time=0;
 					result.title=document.querySelectorAll("title").item(0).innerHTML;
 					result.dataList=[];
 					var title_all=document.querySelectorAll(".cal-list-right");
@@ -22,8 +23,9 @@ phantom.outputEncoding="gbk";
 						re.content=title_all[i].querySelectorAll(".cal-list-abstract").item(0).innerHTML;
 						result.dataList.push(re);
 					}
+					result.time=Date.now()-t;
 					return JSON.stringify(result,null,4);
-				});
+				},time);
 					//console.log(str);
 				var file=fs.open('result2.json','w');
 				file.write(rs);
@@ -42,7 +44,7 @@ phantom.outputEncoding="gbk";
 					setInterval(function(){
 						var a=document.querySelectorAll(".load-more-btn").item(0);
 						a.click(); 
-					},200);
+					},230);
 					setTimeout(function(){
 							window.callPhantom();
 						},3000);
