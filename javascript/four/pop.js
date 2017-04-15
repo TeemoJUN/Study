b279eb1d32ab36bcf789c7495fd185d51ee3746f
wanzailin <1058794886@qq.com>
2017-04-15 23:31:01 +0800

    four-test

--- javascript/four/pop.js
+++ javascript/four/pop.js
@@ -1,99 +1,86 @@
-//事件绑定函数，兼容浏览器差异
-function addEvent(element, event, listener) {
-    if (element.addEventListener) {
-        element.addEventListener(event, listener, false);
-    }
-    else if (element.attachEvent) {
-        element.attachEvent("on" + event, listener);
-    }
-    else {
-        element["on" + event] = listener;
-    }
+function addEvent(element,event,listener){
+	if(element.addEventListener){
+		element.addEventListener(event,listener,false);
+	}
+	else if(element.attachEvent){
+		element.attachEvent('on'+event,listener);
+	}
+	else{
+		element['on'+event]=listener;
+	}
 }
+function each(arr,fn){
+	for(var cur=0,len=arr.length;cur<len;cur++){
+		fn(arr[cur]);
+	}
+}
+window.onload=function(){
+	var container=document.getElementById('container');
+	var buttonList=document.getElementsByTagName('input');
+	var queue={
+		str:[],
 
-//遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递，后面用
-function each(arr, fn) {
-    for (var cur = 0; cur < arr.length; cur++) {
-        fn(arr[cur], cur);
-    }
-}
- 
-window.onload = function() {
-    var container = document.getElementById("container");
-    var buttonList = document.getElementsByTagName("input");
-    //定义队列的对象
-    var queue = {
-        str: [],
-        
-        leftPush: function(num) {
-            this.str.unshift(num);
-            this.paint();
-        },
-        
-        rightPush: function(num) {
-            this.str.push(num);
-            this.paint();
-        },
-        
-        isEmpty: function() {
-            return (this.str.length == 0);
-        },
-        
-        leftPop: function() {
-            if (!this.isEmpty()) {
-                alert(this.str.shift());
-                this.paint();
-            }
-            else {
-                alert("The queue is already empty!");
-            }
-        },
-        
-        rightPop: function() {
-            if (!this.isEmpty()) {
-                alert(this.str.pop());
-                this.paint();
-            }
-            else {
-                alert("The queue is already empty!");
-            }
-        },
-        
-        paint: function() {
-            var str = "";
-            alert(this.str);
-            each(this.str, function(item){str += ("<div>" + parseInt(item) + "</div>")});
-            container.innerHTML = str;
-            addDivDelEvent();
-        },
-        
-        deleteID: function(id) {
-            console.log(id);
-            this.str.splice(id, 1);
-            this.paint();
-        }
-        
-    }
-    
-    //为container中的每个div绑定删除函数
-    function addDivDelEvent() {
-        for (var cur = 0; cur < container.childNodes.length; cur++) {
-            
-            //这里要使用闭包，否则永远绑定到指定div上的delete函数的参数永远等于跳出时的cur值(length);
-            addEvent(container.childNodes[cur], "click", function(cur) {
-                return function(){return queue.deleteID(cur)};
-            }(cur));
-        }
-    }
-    
-    //为4个按钮绑定函数
-    addEvent(buttonList[1], "click", function() {
+		leftPush:function(num){
+			this.str.unshift(num);
+			this.paint();
+		},
+
+		rightPush:function(num){
+			this.str.push(num);
+			this.paint();
+		},
+		isEmpty:function(){
+			return (this.str.length==0);
+		},
+		leftPop:function(){
+			if(!this.isEmpty()){
+				console.log(this.str.shift());
+				this.paint();
+			}
+			else{
+				console.log('队列已空!');
+			}
+		},
+		rightPop:function(){
+			if(!this.isEmpty()){
+				console.log(this.str.pop());
+				this.paint();
+			}
+			else{
+				console.log('已空');
+			}
+		},
+		paint:function(){
+			var str='';
+			each(this.str,function(item){
+				str+=('<div>'+parseInt(item)+'</div>')
+			});
+			container.innerHTML=str;
+			addDivDelEvent();
+		},
+		deleteID:function(id){
+			console.log(id);
+			this.str.splice(id,1);
+			this.paint();
+		}
+	}
+	function addDivDelEvent(){
+		for(var cur=0,len=container.childNodes.length;cur<len;cur++){
+			addEvent(container.childNodes[cur],'click',function(cur){
+				return function(){
+					return queue.deleteID(cur);
+				}(cur);
+			})
+
+		}
+	}
+	addEvent(buttonList[1], "click", function() {
         var input = buttonList[0].value;
         if ((/^[0-9]+$/).test(input)) {
             queue.leftPush(input);
         }
         else {
-            alert("Please enter an interger!");
+            alert("输入数字");
         }
     });
     addEvent(buttonList[2], "click", function() {
@@ -102,12 +89,9 @@ window.onload = function() {
             queue.rightPush(input);
         }
         else {
-            alert("Please enter an interger!");
+            alert("输入数字");
         }
     });
     addEvent(buttonList[3], "click", function(){queue.leftPop()});
     addEvent(buttonList[4], "click", function(){queue.rightPop()});
 }
-
-
-// 
